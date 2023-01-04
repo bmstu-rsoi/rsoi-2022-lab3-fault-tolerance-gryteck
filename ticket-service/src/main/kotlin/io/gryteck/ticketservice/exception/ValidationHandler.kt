3ecddd34eedbc501570848b87a9491f57bcac619
gryteck <gryteck@vk.com>
2023-01-04 16:42:02 +0300

    added lab 2

---
+++ ticket-service/src/main/kotlin/io/gryteck/ticketservice/exception/ValidationHandler.kt
@@ -0,0 +1,9 @@
+package io.gryteck.ticketservice.exception
+
+import io.gryteck.common.exception.CommonExceptionHandler
+import org.springframework.context.annotation.Import
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+@Import(CommonExceptionHandler::class)
+class ValidationHandler
